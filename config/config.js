bec26ffcf30caa00ff2337db9cf7fee946ef8387
Taegyeong Han <65719512+Ronis-han@users.noreply.github.com>
2021-09-17 01:30:20 +0900

    aa

--- config/config.js
+++ config/config.js
@@ -7,7 +7,8 @@ module.exports ={
       "database": "memoApp",
       "host": "127.0.0.1",
       "dialect": "postgres",
-      "port":5432
+      "port":5432,
+      "operatorsAliases": false
     },
     "test": {
       "username": "postgres",
